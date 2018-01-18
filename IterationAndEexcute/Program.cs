446043e3540f031047e96ebb6f8fbc47aac2c94a
Phạm Đặng Hoàng Giang <phamdanghoanggiang@gmail.com>
2018-01-18 22:00:35 +0700

    123

--- IterationAndEexcute/Program.cs
+++ IterationAndEexcute/Program.cs
@@ -1,15 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace IterationAndEexcute
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            var arr = Enumerable.Range(1, 10).Cast<object>();
+
+            var result = I.Iterate(arr, new object[] {"a", "b"}, (item, list) => item + (string) list[0] + (string) list[1]).Cast<string>();
+            Console.WriteLine(string.Join(", ", result));
+
+            Console.ReadKey();
+        }
+    }
+
+    public class I
+    {
+        public static IList<object> Iterate(IEnumerable<object> items, object[] args, Func<object, object[], object> executeFunc)
+        {
+            return items.Select(item => executeFunc(item, args)).ToList();
         }
     }
 }
